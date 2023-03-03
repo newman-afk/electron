d5630281bc32b881f5943bf5cd7cd71528b53f62
newman-afk <84593706+newman-afk@users.noreply.github.com>
2023-03-03 20:28:46 +0800

    Finish this App
    Fllow youtube tutorial

--- main.js
+++ main.js
@@ -1,1 +1,112 @@
-console.log("haha");
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+const resizeImg = require("resize-img");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+
+const isMac = process.platform === "darwin";
+const isDev = process.env.NODE_ENV !== "production";
+
+let mainWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: isDev ? 1000 : 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+  //   Open devtools window
+  if (isDev) {
+    console.log("Open devtools window");
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+}
+
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About",
+    width: 300,
+    height: 300,
+  });
+
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  mainWindow.on("closed", () => (mainWindow = null));
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+  });
+});
+
+const template = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  { role: "fileMenu" },
+  ...(!isMac && [
+    {
+      label: "Help",
+      submenu: [
+        {
+          label: "About",
+          click: createAboutWindow,
+        },
+      ],
+    },
+  ]),
+];
+
+ipcMain.on("image:resize", (e, options) => {
+  options.dest = path.join(os.homedir(), "imageresizer");
+  resizeImage(options);
+});
+
+async function resizeImage({ width, height, dest, imgPath }) {
+  try {
+    const newPath = await resizeImg(fs.readFileSync(imgPath), {
+      width: +width,
+      height: +height,
+    });
+
+    const filename = path.basename(imgPath);
+
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+
+    fs.writeFileSync(path.join(dest, filename), newPath);
+
+    mainWindow.webContents.send("image:donw");
+
+    shell.openPath(dest);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.on("window-all-closed", () => {
+  if (isMac) app.quit();
+});
